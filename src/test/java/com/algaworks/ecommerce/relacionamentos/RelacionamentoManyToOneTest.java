1148fd87aeac4bc8ddf1562260a2097fea4cf6fc
Daverson <dds.saldanha@gmail.com>
2020-07-13 15:56:56 -0300

    Revert "Revert "Aula 6.5""
    This reverts commit ea46de800181330990d5a77765cf8919073ce776.

--- src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
+++ src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
@@ -40,19 +40,28 @@ public class RelacionamentoManyToOneTest extends EntityManagerTest {
         pedido.setTotal(BigDecimal.TEN);
         pedido.setCliente(cliente);
 
+        entityManager.persist(pedido);
+
+        // Pode ser que logo ao executar o método "persist" o JPA já faça a sincronização com a base.
+        // Mas caso isso não aconteça, o flush garante a sincronização.
+        entityManager.flush();
+
         ItemPedido itemPedido = new ItemPedido();
+//        itemPedido.setPedidoId(pedido.getId()); IdClass
+//        itemPedido.setProdutoId(produto.getId()); IdClass
+        itemPedido.setId(new ItemPedidoId(pedido.getId(), produto.getId()));
         itemPedido.setPrecoProduto(produto.getPreco());
         itemPedido.setQuantidade(1);
         itemPedido.setPedidoid(pedido);
         itemPedido.setProdutoid(produto);
 
-        entityManager.persist(pedido);
         entityManager.persist(itemPedido);
         getTransacao();
 
         entityManager.clear();
 
-        ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
+        ItemPedido itemPedidoVerificacao = entityManager.find(
+                ItemPedido.class, new ItemPedidoId(pedido.getId(), produto.getId()));
         Assert.assertNotNull(itemPedidoVerificacao.getPedidoid());
         Assert.assertNotNull(itemPedidoVerificacao.getProdutoid());
 
