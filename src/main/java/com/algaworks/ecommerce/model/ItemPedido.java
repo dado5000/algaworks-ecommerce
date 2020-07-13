ea46de800181330990d5a77765cf8919073ce776
Daverson <dds.saldanha@gmail.com>
2020-07-13 15:54:02 -0300

    Revert "Aula 6.5"
    This reverts commit 95eab2fae345b3e9caef40a1817d73a6407a5632.

--- src/main/java/com/algaworks/ecommerce/model/ItemPedido.java
+++ src/main/java/com/algaworks/ecommerce/model/ItemPedido.java
@@ -13,15 +13,23 @@ import java.math.BigDecimal;
 @Table(name = "item_pedido")
 public class ItemPedido {
 
-    @EmbeddedId
-    private ItemPedidoId id;
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+//    @Column(name = "pedido_id")
+//    private Integer pedidoId;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "pedido_id", insertable = false, updatable = false)
+    @JoinColumn(name = "pedido_id")
     private Pedido pedidoid;
 
+//    @Column(name = "produto_id")
+//    private Integer produtoId;
+
     @ManyToOne(optional = false)
-    @JoinColumn(name = "produto_id", insertable = false, updatable = false)
+    @JoinColumn(name = "produto_id")
     private Produto produtoid;
 
     @Column(name = "preco_produto")
@@ -30,8 +38,4 @@ public class ItemPedido {
     private Integer quantidade;
 
 
-//    @Column(name = "produto_id")
-//    private Integer produtoId; SEM RELACIONAMENTO
-//    @Column(name = "pedido_id")
-//    private Integer pedidoId; SEM RELACIONAMENTO
 }
